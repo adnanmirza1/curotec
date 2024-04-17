d8b0effcd8fa939847bf64d336e697c6e13ed87c
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2024-04-17 15:57:30 +0500

    jj

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,9 +5,9 @@ class ArticlesController < ApplicationController
       # ContactMailer.send_contact_email(@article.name, @article.email, @article.phone, @article.website, @article.description).deliver_now
       # send = SendEmail.new(@article.name, @article.email, @article.description)
       # send.sendmail
-      redirect_to root_path, notice: 'Article was successfully created.'
+      redirect_to root_path, notice: 'Article was successfully created.!!'
     else
-      render 'curotec/contact'
+      render 'curotec/contact'  
     end
   end
 end
