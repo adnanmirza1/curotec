c316ec7ccd93ee010e1cb1b5bb297f678b430ff4
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2024-04-17 13:36:32 +0500

    add service

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,9 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(name: params[:name], description: params[:description], email: params[:email], phone: params[:phone], website: params[:website])
     if @article.save
-      ContactMailer.send_contact_email(@article.name, @article.email, @article.phone, @article.website, @article.description).deliver_now
+      # ContactMailer.send_contact_email(@article.name, @article.email, @article.phone, @article.website, @article.description).deliver_now
+      # send = SendEmail.new(@article.name, @article.email, @article.description)
+      # send.sendmail
       redirect_to root_path, notice: 'Article was successfully created.'
     else
       render 'curotec/contact'
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,3 @@
 class Article < ApplicationRecord
-    validates :email, :phone, :name, :website, presence: true
+    # validates :email, :phone, :name, :website, presence: true
 end
