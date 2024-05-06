880d9ebf79b39da3df291d0fff6896a257ff7ff0
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2024-05-06 17:03:46 +0500

    fixes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def create
-    @article = Article.new(name: params[:name], description: params[:description], email: params[:email], phone: params[:mobile], website: params[:website])
+    @article = Article.new(name: params[:name], description: params[:description], email: params[:email], mobile: params[:mobile], website: params[:website])
     if @article.save
       ContactMailer.send_contact_email(@article.name, @article.email, @article.mobile, @article.website, @article.description).deliver_now
       # send = SendEmail.new(@article.name, @article.email, @article.description)
