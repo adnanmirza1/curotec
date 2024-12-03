643050fd7c0bb7ed323bdc1dce20f7d4134c4251
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2024-12-03 17:35:41 +0500

    div enviorment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,11 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.cache_classes = false
+  config.hosts << "igknighttech.fly.dev"
+  config.hosts << /[a-z0-9]+\.c9users\.dev/
+  config.hosts << "igknighttech.com"
+
   # Do not eager load code on boot.
   config.eager_load = false
 
